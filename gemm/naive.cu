4a4f9a9ae8dd67c772644e3deeb6a14d390b5cc6
Adeniyi Makinde <makindeadeniyi1@gmail.com>
2026-05-15 11:01:37 +0100

    Gemm

--- gemm/naive.cu
+++ gemm/naive.cu
@@ -11,4 +11,16 @@
 #define FLOAT4(value)(reinterpret_cast<float4*> (&(val))[0]) 
 
 
-__global__ void matmul_float32(const float *a, const float *b, float *c, int m, int n, int k)
+__global__ void matmul_float32(const float *a, const float *b, float *c, int M, int N, int K) {
+    int col = threadIdx.x + blockDim.x * blockIdx.x;
+    int row = threadIdx.y + blockDim.y * blockIdx.y;
+
+    if (row < M && col < N) {
+        float accum = 0.0f;
+
+        for (int i = 0; i < K; i++) {
+            accum += a[row * K + i] * b[i * N + col];
+        }
+
+    }
+}
