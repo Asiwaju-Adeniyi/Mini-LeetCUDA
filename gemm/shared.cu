c346c046c98398ccae3876f181236593f9547533
Adeniyi Makinde <makindeadeniyi1@gmail.com>
2026-05-24 23:28:00 +0100

    shared kernel

---
+++ gemm/shared.cu
@@ -0,0 +1,29 @@
+#include <algorithm> 
+#include <float.h>
+#include <cuda_runtime.h>
+#include <stdio.h>
+#include <stdlib.h> 
+#include <vector>
+#include <iostream>
+#include <cmath> 
+
+#define CEIL_DIV(M, N) (((M) + (N)-1)/(N))
+
+template <const int BLOCKSIZE>
+
+__global__ void shared_sgemm(int M, int N, int K, float* A, float *B, float *C) {
+    const uint cRow = blockIdx.x;
+    const uint cCol = blockIdx.y;
+
+    __shared__ float As[BLOCKSIZE * BLOCKSIZE];
+    __shared__ float Bs[BLOCKSIZE * BLOCKSIZE];
+
+    const uint threadRow = threadIdx.x / BLOCKSIZE;
+    const uint threadCol = threadIdx.x % BLOCKSIZE;
+
+    A += cRow * BLOCKSIZE * K;
+    B += cCol * BLOCKSIZE;
+    C += cRow * BLOCKIZE * N + cCol * BLOCKSIZE;
+}
+
+
