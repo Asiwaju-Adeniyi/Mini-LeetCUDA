b2366a2db755d6ad1c8e1efe6ba8c76c301a76e9
Adeniyi Makinde <makindeadeniyi1@gmail.com>
2026-05-25 18:41:38 +0100

    register tiling

--- gemm/1D_blocktiling.cu
+++ gemm/1D_blocktiling.cu
@@ -21,12 +21,39 @@ __global__ void 1D_blocktiled(int M, int N, int K, float *a, float*b, float* c) 
 
     static_assert((BM * BK) == blockIdx.x, "You sure?");
     static_assert((BN * BK) == blockIdx.x, "You damn sure?");
+    const uint innerRowA = threadRow / BM;
+    const uint innerColA = threadCol / BK;
+    const uint innerRowB = threadRow / BK;
+    const uint innerColB = threadCol / BN;
 
     A += cRow * BK * K;
     B += cCol * BN;
     C += cRow * BK * K + cCol * BN;
 
-    float accum = {0.0;
 
-        for (int bkIdx = 0; bkIdx < K)
+
+    float accum[TM] = {0.0};
+
+        for (int bkIdx = 0; bkIdx < K; bkIdx++) {
+            sA[threadRow * BK + threadCol] = A[threadRow * K + threadCol];
+            sB[threadRow * BN + threadCol] = A[threadRow * N + threadCol];
+
+            __syncthreads();
+
+            A += BK;
+            B += BN * N;
+
+            for (int id = 0; id < BK; id++) {
+                float accumB = sB[innerRow * BN + innerCol];
+
+                for (int dot = 0; dot < BK; dot++) {
+                   accum[dot] += sA[(threadRow * TM + dot) * K + id] * accumB;
+                } 
+            }
+             __syncthreads();
+        }
+
+        for (int resIdx = 0; resIdx < TM; resIdx++) {
+            C[(threadRow * TM + resIdx) * N + threadCol] = accum[resIdx];
+        }
 }
