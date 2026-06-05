453503a854dd8bef947b421b7640dab293e1703e
Adeniyi Makinde <makindeadeniyi1@gmail.com>
2026-06-05 15:56:28 +0100

    error macro

--- traininloop.cu
+++ traininloop.cu
@@ -4,3 +4,18 @@
 #include <time.h>
 #include <string.h>
 #include <cuda_runtime.h>
+
+#include <cuda_runtime.h>
+
+#define CUDA_CHECK(call)                                      \
+do {                                                          \
+    cudaError_t error = call;                                 \
+    if (error != cudaSuccess) {                               \
+        fprintf(stderr, "CUDA error at %s:%d: %s\n",         \
+            __FILE__, __LINE__, cudaGetErrorString(error));   \
+        cudaDeviceReset();                                    \
+        exit(EXIT_FAILURE);                                   \
+    }                                                         \
+} while(0)
+
+
