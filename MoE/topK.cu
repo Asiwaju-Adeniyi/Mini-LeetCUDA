4dce4c220491b50cb871f7e3b428357d2e536cd1
Adeniyi Makinde <makindeadeniyi1@gmail.com>
2026-06-12 01:39:33 +0100

    row based pointer arithmetic for MoE topK kernel routing

--- MoE/topK.cu
+++ MoE/topK.cu
@@ -7,5 +7,11 @@
 #include <iostream>
 
 __global__ topk(const float* input, float *values, float *indices, int batch_size, int n, int k) {
-    
+    int batch_idx = blockIdx.x;
+
+    if (batch_idx < batch_size) {
+        const float* input_row = input + batch_idx * n;
+        float* v_row = values + batch_idx * k;
+        float* i_row = indices + batch_idx * k;
+    }
 }
