d6b9ab80fd5fab5204b3a207a39f8aa05d0a421d
Adeniyi Makinde <makindeadeniyi1@gmail.com>
2026-06-18 15:54:40 +0100

    tracking addresses

--- MoE/GEMV.cu
+++ MoE/GEMV.cu
@@ -21,3 +21,18 @@ __global__ void gemv_kernel(const float *A, const float *x, float* y, int batch,
 
     y[batch_idx * M + row] = accum;
 }
+
+__global__ void gemV_address_tracking(const float *a, const float *b, float *c, int M, int N) {
+    const uint row = blockIdx.y * blockDim.y + threadIdx.y;
+    if (row < M) {
+        float accum = 0.0f;
+        for (int i = 0; i < N; i++) {
+            if (row < 3 && i == 0) {
+                printf("thread %u: a address = %p, b address = %p\n",
+                       row, (void*)&a[row * N + i], (void*)&b[i]);
+            }
+            accum += a[row * N + i] * b[i];
+        }
+        c[row] = accum;
+    }
+}
