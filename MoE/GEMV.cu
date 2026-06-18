491613f273fcc6949affaf09881beebc52005ae7
Adeniyi Makinde <makindeadeniyi1@gmail.com>
2026-06-18 23:57:31 +0100

    code for shuffle primitive

--- MoE/GEMV.cu
+++ MoE/GEMV.cu
@@ -22,16 +22,26 @@ __global__ void gemv_kernel(const float *A, const float *x, float* y, int batch,
     y[batch_idx * M + row] = accum;
 }
 
+__device__ __forceinline__ float warpReduceSum(float val){
+    uint WarpSize = 32;
+    #pragma unroll
+
+    for (int delta = WarpSize >> 1; delta >= 0; delta >>= 1) {
+        val += shfl_down_sync(0xfffffff, val, delta);
+    }
+    return val;
+}
+
 __global__ void gemV_address_tracking(const float *a, const float *b, float *c, int M, int N) {
-    const uint row = blockIdx.y * blockDim.y + threadIdx.y;
+    const uint row = blockDim.x;
+    
     if (row < M) {
-        float accum = 0.0f;
+        float PartialAccum = 0.0f;
         for (int i = 0; i < N; i++) {
             if (row < 3 && i == 0) {
                 printf("thread %u: a address = %p, b address = %p\n",
                        row, (void*)&a[row * N + i], (void*)&b[i]);
             }
-            accum += a[row * N + i] * b[i];
         }
         c[row] = accum;
     }
