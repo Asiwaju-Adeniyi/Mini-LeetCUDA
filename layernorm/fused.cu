feca1b21d47f4c21328670b8eb263b958c276b13
Adeniyi Makinde <makindeadeniyi1@gmail.com>
2026-07-10 23:33:46 +0100

    removed pragma unroll

--- layernorm/fused.cu
+++ layernorm/fused.cu
@@ -12,7 +12,7 @@ __device__ __forceinline__ warpReduc(float val) {
     return val;
 }
 
-__global__ void fusedLayerNorm(const float* __restrict__ in, float* __restrict__ out, float* __restrict__ gamma, 
+__global__ void warpLayerNorm(const float* __restrict__ in, float* __restrict__ out, float* __restrict__ gamma, 
     float* __restrict__ beta, float* __restrict__ mean, float* __restrict__ rstd, int N, int C) {
         
         int lane = threadIdx.x % WarpSize;
