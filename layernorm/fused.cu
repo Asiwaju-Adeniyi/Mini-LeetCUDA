2b0319eb1d2f52f94cd5c15347a66e4220b161f9
Adeniyi Makinde <makindeadeniyi1@gmail.com>
2026-07-16 02:36:36 +0100

    boundary check

--- layernorm/fused.cu
+++ layernorm/fused.cu
@@ -97,6 +97,8 @@ __global__ void blockFusedLayerNorm(const float* __restrict__ inp, float* __rest
         float* rowData = shared;           
         float* reducBuf = shared + C;     
 
+        if (idx >= N) {return;};
+
         for (int i = threadIdx.x; i < C; i += blockDim.x) {
             shared[i] = x[i];
         }
