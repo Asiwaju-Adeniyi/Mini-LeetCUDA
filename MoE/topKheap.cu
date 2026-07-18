92cd25bd5f77514b95dd47f5bb0bd29f58177a55
Adeniyi Makinde <makindeadeniyi1@gmail.com>
2026-07-18 03:24:28 +0100

    hippy Yaaa

--- MoE/topKheap.cu
+++ MoE/topKheap.cu
@@ -26,4 +26,13 @@ __device__ __forceinline__ void heapify_down(IndexValue* heap, int k, int size) 
 
     if (left < size && heap[left] < heap[smallest]) {smallest = left;}
     if (right < size && heap[right] < heap[smallest]) {smallest = right;}
+    
+    if (smallest != k) {
+        IndexValue temp = heap[k];
+        heap[k] = heap[smallest];
+        heap[smallest] = temp;
+
+        heapify_down(heap, smallest, size);
+    }
+
 }
