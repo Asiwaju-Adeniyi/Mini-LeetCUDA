1526b393999afb480faa1416ab68545a858efb52
Adeniyi Makinde <makindeadeniyi1@gmail.com>
2026-07-20 14:53:19 +0100

    moving around

--- MoE/topKheap.cu
+++ MoE/topKheap.cu
@@ -20,19 +20,21 @@ struct IndexValue (
 
 
 __device__ __forceinline__ void heapify_down(IndexValue* heap, int k, int size) {
-    int smallest = k;
-    int left = 2 * k + 1;
-    int right = 2 * k + 2;
+int smallest = k;
+int left = 2 * k + 1;
+int right = 2 * k + 2;
 
-    if (left < size && heap[left] < heap[smallest]) {smallest = left;}
-    if (right < size && heap[right] < heap[smallest]) {smallest = right;}
+if (left < size && heap[left] < heap[smallest]) smallest = left;
+if (right < size && heap[right] < heap[smallest]) smallest = right;
+
+if (smallest != k) {
+    IndexValue accum = heap[k];
+    heap[k] = heap[smallest];
+    heap[smallest] = accum;
+
+    heapify_down(heap, k, size);
+
+}
     
-    if (smallest != k) {
-        IndexValue temp = heap[k];
-        heap[k] = heap[smallest];
-        heap[smallest] = temp;
-
-        heapify_down(heap, smallest, size);
-    }
 
 }
