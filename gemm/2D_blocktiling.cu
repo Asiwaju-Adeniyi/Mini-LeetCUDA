4310084d6dfc7751e72aabe3edc9eb02ae8d2dcb
Adeniyi Makinde <makindeadeniyi1@gmail.com>
2026-07-26 00:12:04 +0100

    ...

--- gemm/2D_blocktiling.cu
+++ gemm/2D_blocktiling.cu
@@ -17,7 +17,7 @@ __global__ void __launch_bounds__((BM * BN) / (TM * TN), 1) 2D_blocktiled(int M,
     const uint outputPB = BM * BN;
     const uint tpB = outputPB / TM * TN;
 
-    static_assert((tpB == blockDim.x), "Not threads per block. Recheck!!");
+    static_assert((tpB == blockDim.x), "threads per block not equal to block Dimension. Recheck!!");
 
     const uint threadRow = threadIdx.x / TM * TN;
     const uint threadCol = threadIdx.x % TM * TN;
