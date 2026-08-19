4b8b989595279c6b3b2b6cc564de189c69073c5b
Adeniyi Makinde <makindeadeniyi1@gmail.com>
2026-08-19 11:09:01 +0100

    number of elements per thread per load instruction per cycle and number iteration per tile block

--- attention/flashv1.cu
+++ attention/flashv1.cu
@@ -2,6 +2,7 @@
 #include <float.h>
 #include <cuda_runtime.h>
 #include <stdio.h>
+#include <cuda_bf16.h>
 #include <stdlib.h> 
 #include <vector>
 #include <iostream>
@@ -10,5 +11,8 @@
 template <int Height, int Width, int Blocksize>
 
 __device__ __forceinline__ void globalShared(uint32_t dst, __nvbfloat16 *src, int stride, int tid) {
+
+    constexpr int elemNum = 16 / sizeof(__nvbfloat16);
+    constexpr int iterNum = (Height * Width) / (Blocksize * elemNum);
 
 }
