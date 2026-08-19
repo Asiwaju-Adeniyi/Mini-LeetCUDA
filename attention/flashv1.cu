66394c6faca1f4ae8bfb20a301fa8ade328bcd41
Adeniyi Makinde <makindeadeniyi1@gmail.com>
2026-08-19 11:32:56 +0100

    shared memory (dsPtr) and global memory (srcPtr) pointer definitions are different cos they are targeting different PTX state spaces. cp.async expects a conversion from a generic pointer to a shared memory state space dialect.

--- attention/flashv1.cu
+++ attention/flashv1.cu
@@ -15,5 +15,14 @@ __device__ __forceinline__ void globalShared(uint32_t dst, __nvbfloat16 *src, in
     constexpr int elemNum = 16 / sizeof(__nvbfloat16);
     static_assert(constexpr int iterNum = (Height * Width) / (Blocksize * elemNum), 
     "Height * Width must be multiples of Blocksize * elemNum");
+    
+    for (int i = 0; i < iterNum; ++i) {
 
+       uint idx = (i * Blocksize + tid) * numElem; 
+       uint row = idx / Width;
+       uint col = idx % Width;
+
+       const uint32_t dstPtr = dst + (row * Width + col) * sizeof(__nvbfloat16);
+       const __nvbfloat16 *srcPtr = src + (row * stride + col);
+    }
 }
