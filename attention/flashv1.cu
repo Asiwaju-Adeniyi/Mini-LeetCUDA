5df2961833b8ef84073ecc0b977e53f71ad88603
Adeniyi Makinde <makindeadeniyi1@gmail.com>
2026-09-16 00:35:03 +0100

    changes

--- attention/flashv1.cu
+++ attention/flashv1.cu
@@ -22,7 +22,6 @@ void fmhaForwardDevice(int numQueries, int numKeys, int numHeads, int batchSize,
   using OperandB = StorageT;
   using Accumulator = AccumT; 
 
-}
 
 auto tileShapeQ = make_shape(TileQ{}, TileD{});
 auto smemLayoutQ =
@@ -30,9 +29,14 @@ auto smemLayoutQ =
 Layout gmemLayoutQ =
     make_layout(make_shape(qRows, headDim, heads, batch),
                 make_stride(headDim * heads, 1, headDim, heads * qRows * headDim));
-Tensor qGmemTensor = make_tensor(qPtr, gmemLayoutQ);
+Tensor qGmemTensor = make_tensor(qGlobal, gmemLayoutQ);
 auto tmaQ =
     make_tma_copy(SM90_TMA_LOAD{}, qGmemTensor, smemLayoutQ, tileShapeQ, Int<1>{});
+
+    
+
+}
+
 
 
   
